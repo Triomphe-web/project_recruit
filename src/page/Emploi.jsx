45b8c488b31eaaac6a17201e1c740283660fee84
akane <f23triomphe@gmail.com>
2024-02-09 10:44:23 +0100

    ajout de search box

--- src/page/Emploi.jsx
+++ src/page/Emploi.jsx
@@ -2,30 +2,43 @@ import React from "react";
 import Jobs from "../components/Jobs";
 import hiring from "../img/hiring.png";
 import '../css/bootstrap.css'
+import '../css/index.css'
+import Header from "../components/Header";
+
 
 export default function Emploi() {
     return (
-        <div className="ml-5 mt-4">
-            <Jobs
-                image={hiring}
-                name="Développeur .NET"
-                desc="Nous recrutons plusieur Développeur .NET"
-                
-            />
-
-            <Jobs
-                image={hiring}
-                name="Développeur React"
-                desc="Nous recrutons plusieur Développeur .NET"
-                
-            />
-
-            <Jobs
-                image={hiring}
-                name="Développeur Python"
-                desc="Nous recrutons plusieur Développeur .NET"
-                
-            />
+        <div>
+            <Header />
+            <div class="search-container container">
+            <form class="input-group">
+                <input type="text" class="form-control rounded-end" placeholder="Rechercher..." />
+                <button type="submit" class="btn btn-primary rounded-start">Rechercher</button>
+            </form>
         </div>
+            <div className="ml-5 mt-4">
+                <Jobs
+                    image={hiring}
+                    name="Développeur .NET"
+                    desc="Nous recrutons plusieur Développeur .NET"
+                    
+                />
+
+                <Jobs
+                    image={hiring}
+                    name="Développeur React"
+                    desc="Nous recrutons plusieur Développeur .NET"
+                    
+                />
+
+                <Jobs
+                    image={hiring}
+                    name="Développeur Python"
+                    desc="Nous recrutons plusieur Développeur .NET"
+                    
+                />
+            </div>
+        </div>
+        
     )
 }
